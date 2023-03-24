ead6637db103d3c898f48bcd68b3384008cb6df6
José J. da Silva Neto <zeneto2508@gmail.com>
2023-03-24 19:22:29 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+const clock = setInterval(function time() {
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let sec = dateToday.getSeconds();
+
+    if (hr <10) hr = '0' + hr;
+
+    if (min <10) hr = '0' + min;
+
+    if (sec <10) hr = '0' + sec;
+
+    hours.textContent = hr;
+    minutes.textContent = min;
+    seconds.textContent = sec;
+});
